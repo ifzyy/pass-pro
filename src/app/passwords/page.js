3e0931c1ed282b8ce0a15f566b812ea71e74d57b
johnson <johnsonnifemi8@gmail.com>
2023-10-13 02:52:31 +0100

    add splash screen

--- src/app/passwords/page.js
+++ src/app/passwords/page.js
@@ -1,17 +1,25 @@
-
+"use client";
 import Image from "next/image";
 import { HiOutlineUserGroup } from "react-icons/hi";
 import { AiOutlineAppstore } from "react-icons/ai";
 import { LiaWalletSolid } from "react-icons/lia";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
-const Passwords= () => {
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect, useState } from "react";
+const Passwords = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
     <main className="bg-[#9046CF1A]">
+      <div>
       <header className="flex justify-between p-[20px] mb-[30px]">
-        <div  className="font-bold">
-        Passwords
-        </div>
+        <div className="font-bold">Passwords</div>
 
         <Image src="/images/Notification.svg" width={20} height={20} />
       </header>
@@ -27,66 +35,105 @@ const Passwords= () => {
         />
         <CiSearch className="text-[20px] ml-[-30px]" />
       </label>
-
-      
+      </div>
+  
 
       <div className="bg-[#fefefe] p-[20px] mb-[80px]">
         <h2 className="font-bold mb-[20px] mt-[20px]">Socials</h2>
 
         <div className="flex flex-col gap-[20px]">
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/instagram.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Instagram</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi8@gmail.com</p>
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/instagram.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Instagram</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi8@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/gmail.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Snapchat</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi8@gmail.com</p>
+          )}
+          
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/gmail.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Snapchat</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi8@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/twitter.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Twitter</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi8@gmail.com</p>
+          )}
+        
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/twitter.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Twitter</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi8@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
-
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/instagram.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Instagram</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi8@gmail.com</p>
+          )}
+          
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/instagram.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Instagram</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi8@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
+          )}
         </div>
         <h2 className="font-bold mb-[20px] mt-[20px]">Apps</h2>
 
         <div className="flex flex-col gap-[20px]">
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/figma.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Figma</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi2003@gmail.com</p>
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/figma.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Figma</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi2003@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
-          <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
-            <Image src="/images/figma.svg" width={40} height={40} />
-            <div>
-              <h2 className="font-bold text-[#1A181B]">Figma</h2>
-              <p className="text-[#1A181B80] text-[14px]">johnsonnifemi8@gmail.com</p>
+          )}
+          
+          {loading ? (
+            <Skeleton className="shadow-md p-[20px] bg-[white] flex items-center justify-between" />
+          ) : (
+            <div className=" shadow-md p-[20px] bg-[white] flex items-center justify-between">
+              <Image src="/images/figma.svg" width={40} height={40} />
+              <div>
+                <h2 className="font-bold text-[#1A181B]">Figma</h2>
+                <p className="text-[#1A181B80] text-[14px]">
+                  johnsonnifemi8@gmail.com
+                </p>
+              </div>
+              <BsThreeDotsVertical />
             </div>
-            <BsThreeDotsVertical />
-          </div>
+          )}
         </div>
       </div>
     </main>
---
+++ src/app/splashscreen.js
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
+import anime from "animejs";
+import { preLoaderAnim } from "./animations";
+import TypeWriter from "typewriter-effect";
+function SplashScreen({ finishLoading }) {
+  const [firstClass, setFirstClass] = useState("");
+  const [secondClass, setSecondClass] = useState("");
+  const [firstFade, setFirstFade] = useState("");
+  const [secondFade, setSecondFade] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
+  const [overallFade, setOverallFade] = useState("");
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
+    loader.add({
+      targets: "#logo",
+      delay: 0,
+      scale: 1.5,
+      duration: 6000,
+      easing: "easeInOutExpo",
+    });
+  };
+  useEffect(() => {
+    preLoaderAnim();
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    animate();
+    return () => clearTimeout(timeout);
+  }, []);
+  return (
+    <div className="preloader">
+      <h1 id="logo" className="texts-container">
+        <span>Pass</span>
+        <span>Pro</span>
+      </h1>
+      <TypeWriter
+        options={{
+          strings: ["All In One"],
+          autoStart: true,
+          loop: true,
+          className: "type"
+        }}
+      />
+    </div>
+  );
+}
+
+export default SplashScreen;
